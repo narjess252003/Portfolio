d75ff064a6b2aebb3f35c4a14d79f4ff73ff3699
Beltaief Narjess <narjessbeltaief14@gmail.com>
2025-07-05 17:36:02 +0100

    .

---
+++ script.js
@@ -0,0 +1,8 @@
+function ToggleMenu() {
+    const menu = document.querySelector('.menu-links');
+    const icon = document.querySelector('.mobile-icon');
+    menu.classList.toggle('open');
+    icon.classList.toggle('open');
+
+    
+}
